1397637a15aacde9367ee879e6219a02a7911a74
kate <katezraly@gmail.com>
2022-10-30 15:53:22 -0700

    added injection script for navbar and footer

---
+++ scripts/inject_navfoot.js
@@ -0,0 +1,8 @@
+// Injects the navbar, and footer into the HTML doc
+function inject_navfoot() {
+    console.log($('#nav').load('./text/nav_template.html'));
+    console.log($('#foot').load('./text/foot_template.html'));
+}
+
+// Calls the inject_navfoot function
+inject_navfoot();
