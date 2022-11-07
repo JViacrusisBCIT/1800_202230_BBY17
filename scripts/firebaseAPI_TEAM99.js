8edc949ecb2736c9d0b369cb552da75699f19c0e
JViacrusisBCIT <jviacrusis1@my.bcit.ca>
2022-11-06 22:04:13 -0800

    added a user login/signup process with placeholder initialization data
    - main.html was created to show you the 4 tile options plus a tutorial
    - index.html now prompts you to log in
    - login.html is the actual login/s
    - js files added for connetion to firebase and firestore
    - data created when a new user is created is placeholder for now

---
+++ scripts/firebaseAPI_TEAM99.js
@@ -0,0 +1,19 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+const firebaseConfig = {
+  apiKey: "AIzaSyCwdmFMGBQ-v4KXMCDzh2EF_r9fIiyDzC0",
+  authDomain: "iinsight-32b02.firebaseapp.com",
+  projectId: "iinsight-32b02",
+  storageBucket: "iinsight-32b02.appspot.com",
+  messagingSenderId: "120022641448",
+  appId: "1:120022641448:web:dca1161bba52396426fe1d",
+  measurementId: "G-KKQMGYJT9W"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
