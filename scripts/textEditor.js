f0151aa01b4cd23ae81dc9a9e70560b0d48b7f54
kate <katezraly@gmail.com>
2022-11-23 12:09:53 -0800

    text editor contents written to db when click save
    on clicking the "save" button...
    - Quill converts the text editor's contents to a Delta object
    - we convert the Delta object to JSON
    - we write the JSON to the database

--- scripts/textEditor.js
+++ scripts/textEditor.js
@@ -1,3 +1,50 @@
+
+var params = new URL(window.location.href);
+var fileID = params.searchParams.get("fileid");
+
+
 var quill = new Quill('#editor', {
   theme: 'snow'
 });
+
+
+
+function loadText() {
+
+  // JSON
+  let json = "text";
+
+  // Converts to Delta Object
+  delta = JSON.parse(json);
+
+}
+
+
+// Takes the content in the text editor and writes it to the database.
+function saveText() {
+
+  // Delta Object
+  let delta = quill.getContents();
+  
+  // Convert to JSON
+  let json = JSON.stringify(delta);
+
+  db.collection("files")
+  .where("fileid", "==", fileID)
+  .get()
+      .then(files => {
+
+        files.forEach(file => {
+
+          db.collection("files").doc(file.id).update({
+            content: json
+          });
+
+        });
+
+      });
+
+}
+
+
+//loadText();
