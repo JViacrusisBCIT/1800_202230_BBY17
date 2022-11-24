ce069932288de3d1b4d727835a1415566d219d1c
Justin Viacrusis <jviacrusis1@my.bcit.ca>
2022-11-24 11:57:16 -0800

    Decomposed large method in classrooms.js and students.js, Added ability to add a classroom with many students via csv file.
    Decomposed large method in classroom.js and students.js
    	- Confirm button event listener was doing too many things
    	- broke it down into createStudent/createClassroom, getTeacherId and the main callback function
    
    Added ability to add a classroom with many students via csv file.
    	- given an optional csv file input, the add new classroom button will now create the classroom and populate it with students from the list

--- scripts/classrooms.js
+++ scripts/classrooms.js
@@ -1,61 +1,122 @@
 var newClassroomModal = document.getElementById('new-classroom-modal')
 var bsNewClassroomModal = new bootstrap.Modal(document.getElementById('new-classroom-modal'), {
-  keyboard: false
+    keyboard: false
 })
 var confirmCreateButton = document.getElementById('confirm-create-button')
+
+// toggle classroom creation modal
 newClassroomModal.addEventListener('show.bs.modal', function (event) {
-  console.log("modal toggled");
-  let modalTitle = newClassroomModal.querySelector('.modal-title');
-  modalTitle.textContent = 'New Classroom';
+    console.log("modal toggled");
+    let modalTitle = newClassroomModal.querySelector('.modal-title');
+    modalTitle.textContent = 'New Classroom';
 })
 
+// confirm classroom creation
 confirmCreateButton.addEventListener('click', function (event) {
-  console.log("confirm button clicked");
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      console.log("user found");
 
-      let className = document.getElementById('class-name').value;
-      document.getElementById('class-name').value = "";
-      bsNewClassroomModal.hide();
-      console.log(user.uid);
-      db.collection("teachers").doc(user.uid).get()
-        .then(function (doc) {
-          if (doc.exists) {
-            console.log("doc exists")
-            console.log(doc.data().teacherid)
-            return doc.data().teacherid;
-          } else {
-            console.log("teacher doc not found");
-          }
-        }).then(function (teacherID) {
-          db.collection("classes").add({          
-            name: className,                                           
-            classid: makeid(),                    
-            teacherid: teacherID,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-          }).then(function () {
-            console.log("New class added to firestore");
-          })
-            .catch(function (error) {
-              console.log("Error adding new classroom: " + error);
-            })
+    //hides the classroom creation modal
+    bsNewClassroomModal.hide();
+
+    let classNameField = document.getElementById('class-name');
+    let className = classNameField.value;
+    classNameField.value = "";
+
+    getTeacherIdFromCurUser((teacherId) => {
+        // if student list was not provided, just create a single classroom,
+        // otherwise, make a whole bunch of classrooms
+        let fileInputField = document.getElementById('myfile');
+
+        if (fileInputField.files.length == 0) {
+            createClassroom(className, teacherId, () => { });
+        } else {
+            createClassroom(className, teacherId, (classId) => {
+                const reader = new FileReader();
+
+                reader.onload = () => {
+                    console.log(reader.result);
+                    let nameIdPairs = reader.result.split(/,\r\n/);
+                    nameIdPairs.forEach((item) => {
+                        pairArr = item.split(", ");
+                        let studentName = pairArr[0].trim();
+                        let studentId = pairArr[1].trim();
+                        createStudent(studentName, studentId, classId, teacherId);
+                        // deleteStudentByName(studentName);
+                    })
+                };
+
+                reader.readAsText(fileInputField.files[0])
+            });
         }
-        );
-    } else {
-      console.log("no user is logged in")
-    }
-  }
-  )
+    });
+
+
 })
 
-function makeid(length) {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  var idlen = 20;
-  for (var i = 0; i < idlen; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+// creates a classroom in the firesetore database
+function createClassroom(cName, teacherId, callback) {
+    console.log(teacherId);
+    let cId = makeId();
+    db.collection("classes").add({
+        name: cName,
+        classid: cId,
+        teacherid: teacherId,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    callback(cId);
 }
+
+// creates a student in the firesetore database
+function createStudent(sName, sId, classId, teacherId) {
+    console.log(teacherId);
+    db.collection("students").add({
+        name: sName,
+        studentid: sId,
+        classid: classId,
+        teacherid: teacherId,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+}
+
+// get the teacherID of the currently logged on user
+function getTeacherIdFromCurUser(callback) {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            db.collection("teachers").doc(user.uid)
+                .get()
+                .then((doc) => {
+                    callback(doc.data().teacherid);
+                });
+        } else {
+            console.log("no user is logged in");
+            return null;
+        }
+    });
+}
+
+// makes a 20 character string using uppers, lowers, and numbers
+function makeId(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    var idlen = 20;
+    for (var i = 0; i < idlen; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+// function deleteStudentByName(name) {
+
+//     getTeacherIdFromCurUser((teacherId) => {
+//         db.collection("students")
+//             .where("name", "==", name)
+//             .get()
+//                 .then((students) => {
+//                     students.forEach(std => {
+//                         console.log(std.data().name);
+//                         std.ref.delete();
+//                     });
+//                 })
+//     })
+// }
