e8a1a9404acd142790f3edb75c44b9a7221f9f8c
kate <katezraly@gmail.com>
2022-11-27 19:05:11 -0800

    refactored file names to camelCase for consistency

--- scripts/injectTemplates.js
+++ scripts/injectTemplates.js
@@ -1,11 +1,11 @@
 // Injects the navbar, and footer into the HTML doc
-function inject_templates() {
-    console.log($('#nav').load('../text/nav_template.html'));
-    console.log($('#foot').load('../text/foot_template.html'));
+function injectTemplates() {
+    console.log($('#nav').load('../text/navTemplate.html'));
+    console.log($('#foot').load('../text/footTemplate.html'));
 }
 
 // Calls the inject_navfoot function
-inject_templates();
+injectTemplates();
 
 
 
